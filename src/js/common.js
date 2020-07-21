787cf92205fcea2fb308865a105e4ddd2cf6e0fa
voloshkaincn <barvinok.cn@gmail.com>
2020-07-21 23:49:59 +0300

    full page3

---
+++ src/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true,
+        scrollHorizontallyKey: 'YWx2YXJvdHJpZ28uY29tX01mU2MyTnliMnhzU0c5eWFYcHZiblJoYkd4NVNRcg==',
+        slidesNavigation: true,
+        slidesNavPosition: 'top'
+    });
+});
