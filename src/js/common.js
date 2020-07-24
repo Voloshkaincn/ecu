f3c575e5fd5e3b66ebcedb83a478b811e137fdbd
voloshkaincn <barvinok.cn@gmail.com>
2020-07-24 03:48:38 +0300

    HZ zzzz

--- src/js/common.js
+++ src/js/common.js
@@ -1,25 +1,43 @@
 $(document).ready(function () {
-    var slideIndexS = 0,
+    var slideIndex2 = 0,
+        slideIndex3 = 0,
         sliding = false;
     $('#fullpage').fullpage({
-        anchors: ['main', 'steps', 'eating', 'training', 'smoking', 'inheritance', 'footer'],
+        anchors: ['main', 'steps', 'eating', 'inheritance', 'footer'],
         css3: true,
         slidesNavigation: true,
         slidesNavPosition: 'top',
         afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-            slideIndexS = slideIndex + 1;
+            if (index == 2) {
+                slideIndex2 = slideIndex + 1;
+            } else if (index == 3) {
+                slideIndex3 = slideIndex + 1;
+            }
         },
         onLeave: function (index, nextIndex, direction) {
-            if (index == 2 && !sliding) {
-                if (direction == 'down' && slideIndexS < 4) {
+            console.log("index " + index)
+            console.log("slideIndex2 " + slideIndex2)
+            console.log("slideIndex3 " + slideIndex3)
+            if (index == 2) {
+                if (direction == 'down' && slideIndex2 < 4) {
                     $.fn.fullpage.moveSlideRight();
                     return false;
-                } else if (direction == 'up' && slideIndexS > 1) {
+                } else if (direction == 'up' && slideIndex2 > 1) {
+                    $.fn.fullpage.moveSlideLeft();
+                    return false;
+                } else if (direction == 'up' && slideIndex2 < 4) {
+                    $('.fp-slidesNav').removeClass('fp-slidesNav_active');
+                }
+            } else if (index == 3) {
+                if (direction == 'down' && slideIndex3 < 3) {
+                    $.fn.fullpage.moveSlideRight();
+                    return false;
+                } else if (direction == 'up' && slideIndex3 > 1) {
                     $.fn.fullpage.moveSlideLeft();
                     return false;
                 }
-            } else if (sliding) {
-                return false;
+            } else if (index == 1) {
+                $('.fp-slidesNav').addClass('fp-slidesNav_active')
             }
         }
     });
